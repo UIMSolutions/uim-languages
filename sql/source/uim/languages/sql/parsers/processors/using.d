947e30bcd58b98616b9df6cbad7f91b31738e2a7
UIMSolutions <ui.manufaktur@gmail.com>
2025-07-04 07:44:53 +0200

    Add copyright and license information to SQL parser and statement files

--- sql/source/uim/languages/sql/parsers/processors/using.d
+++ sql/source/uim/languages/sql/parsers/processors/using.d
@@ -1,4 +1,9 @@
-module uim.languages.sql.parsers.processors.using;
+mo/****************************************************************************************************************
+* Copyright: © 2018-2025 Ozan Nurettin Süel (aka UIManufaktur)                                                  *
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
+* Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
+*****************************************************************************************************************/
+moduledule uim.languages.sql.parsers.processors.using;
 
 import uim.languages.sql;
 
--- sql/source/uim/languages/sql/statements/statement.d
+++ sql/source/uim/languages/sql/statements/statement.d
@@ -1,4 +1,9 @@
-﻿module uim.languages.sql.statements.statement;
+﻿/****************************************************************************************************************
+* Copyright: © 2018-2025 Ozan Nurettin Süel (aka UIManufaktur)                                                  *
+* License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.         *
+* Authors: Ozan Nurettin Süel (aka UIManufaktur)                                                                *
+*****************************************************************************************************************/
+module uim.languages.sql.statements.statement;
 
 import uim.languages.sql;
 
